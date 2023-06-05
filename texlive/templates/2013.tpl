4f5a14821c3f41bb4c71527cfd8a27d3c8c7e41e
Max Kuznetsov <maks.kuznetsov@gmail.com>
2023-06-05 01:45:47 +0100

    remove templates, remove QEMU

--- texlive/templates/2013.tpl
+++
@@ -1,3 +0,0 @@
-{% with base_image = 'ghcr.io/mkuznets/ubuntu:14.04-2020.11.04' %}
-    {%- include 'common.tpl' %}
-{%- endwith %}
--- texlive/version.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-set -e
-
-TEXLIVE=$(apt-cache show texlive-base | grep Version | sed -r 's/.+([0-9]{4})\..+/\1/')
-DISTRO=$(. /etc/os-release && echo "$ID")
-DAY=$(TZ=Etc/UTC date '+%Y.%m.%d')
-
-echo "${TEXLIVE}-${DISTRO}-${DAY}"
